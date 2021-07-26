9c4eb3cc7e5b46222e20ea285db59d6e2beb7899
GoodbyeNJN <cc@fuckwall.cc>
2021-07-26 10:34:56 +0800

    update keymap for macOS

--- keymaps/default/keymap.c
+++ keymaps/default/keymap.c
@@ -13,9 +13,9 @@ enum custom_keycodes {
 // #define _1 0
 // #define _2 1
 // #define FN 2
-#define MAC 0
+#define MAC 2
 #define MAC_FN 1
-#define WIN 2
+#define WIN 0
 #define WIN_FN 3
 #define WIN_SWAP 4
 #define WIN_FN_SWAP 5
@@ -52,15 +52,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB ,       KC_Q ,   KC_W ,   KC_E ,   KC_R ,   KC_T ,   KC_Y ,   KC_U ,   KC_I ,   KC_O ,   KC_P , KC_LBRC, KC_RBRC,      KC_BSLS,       KC_P7 ,  KC_P8 ,  KC_P9 , KC_PMNS,\
         KC_LCMD,          KC_A ,   KC_S ,   KC_D ,   KC_F ,   KC_G ,   KC_H ,   KC_J ,   KC_K ,   KC_L , KC_SCLN, KC_QUOT,             KC_ENT,       KC_P4 ,  KC_P5 ,  KC_P6 , KC_PPLS,\
         KC_LSFT,             KC_Z ,   KC_X ,   KC_C ,   KC_V ,   KC_B ,   KC_N ,   KC_M , KC_COMM,  KC_DOT, KC_SLSH,          KC_F14,  KC_UP ,       KC_P1 ,  KC_P2 ,  KC_P3 ,         \
-        KC_CAPS,    KC_LOPT,    KC_LCTL,   MO(MAC_FN),        KC_SPC,           KC_RSFT,    KC_ROPT,    KC_RCTL,    KC_RCMD, KC_LEFT, KC_DOWN,      KC_RGHT,  KC_P0 , KC_PDOT, KC_PENT ),
+        KC_CAPS,    KC_LCTL,    KC_LOPT,   MO(MAC_FN),        KC_SPC,           KC_RSFT,    KC_ROPT,    KC_RCTL,    KC_RCMD, KC_LEFT, KC_DOWN,      KC_RGHT,  KC_P0 , KC_PDOT, KC_PENT ),
 
     [MAC_FN] = KEYMAP(
         RESET  ,    DF(MAC),    DF(WIN),    DF(WIN_SWAP),           _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    KC_NLCK,    KC_PSCR,    KC_F20,     KC_F21,\
         _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    KC_HOME,    KC_END,     KC_PGUP,    KC_PGDN,\
         LCAG(KC_LEFT),          KC_F13,     LCA(KC_W),  _______,    _______,    _______,    _______,    KC_HOME,    KC_UP,      KC_END,     _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,\
-        _______,    LCMD(KC_W), _______,    KC_F15,     KC_F17,     _______,    KC_DEL,     KC_LEFT,    KC_DOWN,    KC_RGHT,    KC_BSPC,    _______,    _______,    _______,    _______,    _______,    _______,\
-        _______,    LCA(KC_Z),  KC_F18,     _______,    KC_F16,     _______,    LOPT(KC_LEFT),          LOPT(KC_RGHT),          _______,    _______,    _______,    _______,    KC_VOLU,    _______,    _______,    _______,\
-        _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    LCTL(KC_LEFT),          KC_VOLD,    LCTL(KC_RGHT),          _______,    _______,    _______ ),
+        _______,    LCMD(KC_W), _______,    LCA(KC_D),  KC_F17,     _______,    KC_DEL,     KC_LEFT,    KC_DOWN,    KC_RGHT,    KC_BSPC,    _______,    _______,    _______,    _______,    _______,    _______,\
+        _______,    LCA(KC_Z),  _______,    _______,    KC_F16,     _______,    LOPT(KC_LEFT),          LOPT(KC_RGHT),          _______,    _______,    _______,    _______,    KC_VOLU,    _______,    _______,    _______,\
+        _______,    _______,    _______,    _______,    KC_F15,     _______,    _______,    _______,    _______,    LCTL(KC_LEFT),          KC_VOLD,    LCTL(KC_RGHT),          _______,    _______,    _______ ),
 
     [WIN] = KEYMAP(
         KC_GRV ,  KC_F1 ,  KC_F2 ,  KC_F3 ,  KC_F4 ,  KC_F5 ,  KC_F6 ,  KC_F7 ,  KC_F8 ,  KC_F9 ,  KC_F10,  KC_F11,  KC_F12,  KC_INS,  KC_DEL,      KC_CALC, KC_MPLY, KC_MPRV, KC_MNXT,\
